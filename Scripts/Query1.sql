a37cd359af6e90e3dfc53476f2f3b225bcd5370f
sqldev97 <153027251+sqldev97@users.noreply.github.com>
2023-12-07 22:42:06 +0000

    Joins Done

--- Scripts/Query1.sql
+++ Scripts/Query1.sql
@@ -1,5 +1,25 @@
-select e.firstName, e.lastName , m.firstName, m.lastName
+select e.firstName, e.lastName , e.title, m.firstName AS ManagerFirstName, m.lastName AS ManagerLastName
 from employee e 
-  left join employee m on e.employeeId = m.managerId
+  inner join employee m on e.managerId = m.employeeId
 
-  select * from employee where firstName = 'Wilie'
+select e.firstName, lastName
+ from employee e 
+  left join sales s on e.employeeId = s.employeeId
+where s.salesId is null
+and e.title = 'Sales Person'
+
+SELECT name FROM sqlite_master WHERE type='table';
+
+select salesAmount , soldDate, firstName, lastName, address, city, zipcode, email 
+from sales s 
+inner join customer c on s.customerId = c.customerId
+UNION
+select salesAmount , soldDate, firstName, lastName, address, city, zipcode, email 
+from sales s 
+left join customer c on s.customerId = c.customerId
+where c.customerId is null
+UNION
+select salesAmount , soldDate, firstName, lastName, address, city, zipcode, email 
+from customer c 
+left join sales s on s.customerId = c.customerId
+where s.customerId is null
