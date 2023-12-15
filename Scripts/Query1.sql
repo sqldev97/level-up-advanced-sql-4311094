172ae26830703011d2d94447f4fa853bff5fb607
sqldev97 <153027251+sqldev97@users.noreply.github.com>
2023-12-15 22:46:38 +0000

    pivot

--- Scripts/Query1.sql
+++ Scripts/Query1.sql
@@ -23,3 +23,43 @@ select salesAmount , soldDate, firstName, lastName, address, city, zipcode, emai
 from customer c 
 left join sales s on s.customerId = c.customerId
 where s.customerId is null
+
+select e.employeeId, e.firstName, e.lastName, count(s.salesId) as SalesCount
+from employee e
+  inner join sales s on e.employeeId = s.employeeId
+group by e.employeeId, e.firstName, e.lastName
+order by salescount desc
+
+select e.employeeId, e.firstName, e.lastName, min(s.salesAmount) as Least, MAX(s.salesAmount) AS Most
+from employee e
+  inner join sales s on e.employeeId = s.employeeId
+--where s.soldDate BETWEEN '2022-01-01' and '2022-12-31'
+where s.soldDate >= date('now', 'start of year')
+group by e.employeeId, e.firstName, e.lastName
+
+select e.employeeId, e.firstName, e.lastName
+from employee e
+  inner join sales s on e.employeeId = s.employeeId
+--where s.soldDate BETWEEN '2022-01-01' and '2022-12-31'
+where s.soldDate >= date('now', 'start of year')
+group by e.employeeId, e.firstName, e.lastName
+having count(*) > 5
+
+
+with salesperyear 
+AS
+(select strftime('%Y', soldDate) as salesYear, salesAmount from sales)
+
+select salesYear, sum(salesAmount) AS salesAmount from salesperyear group by salesYear order by salesYear
+
+SELECT employeeId, firstName, lastName, [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12]
+from (
+  select e.employeeId, e.firstName, e.lastName, strftime('%m', salesmonth) as salesmonth
+  from employee e
+    inner join sales s on e.employeeId = s.employeeId
+  where s.soldDate BETWEEN '2021-01-01' and '2021-12-31'
+) as src
+PIVOT (count(*) for salesmonth in ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12])) as pvt
+
+
+group by e.employeeId, e.firstName, e.lastName
